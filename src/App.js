87f829832cfbbba7f0a5cf25bceb60039c17cc81
Rawipol Wajanavisit <oakraw@gmail.com>
2017-12-16 09:53:48 +0700

    link to  home page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
+import { Switch, Route, Link } from 'react-router-dom';
 import { Layout, Menu, Breadcrumb } from 'antd';
+import Home from './pages/Home';
 import './App.css';
 
 const { Header, Content, Footer } = Layout;
@@ -13,21 +15,20 @@ class App extends Component {
           <Menu
             theme="dark"
             mode="horizontal"
-            defaultSelectedKeys={['2']}
+            defaultSelectedKeys={['1']}
             style={{ lineHeight: '64px' }}
           >
-            <Menu.Item key="1">nav 1</Menu.Item>
+            <Menu.Item key="1"><Link to='/'>Home</Link></Menu.Item>
             <Menu.Item key="2">nav 2</Menu.Item>
             <Menu.Item key="3">nav 3</Menu.Item>
           </Menu>
         </Header>
-        <Content style={{ padding: '0 50px' }}>
-          <Breadcrumb style={{ margin: '16px 0' }}>
-            <Breadcrumb.Item>Home</Breadcrumb.Item>
-            <Breadcrumb.Item>List</Breadcrumb.Item>
-            <Breadcrumb.Item>App</Breadcrumb.Item>
-          </Breadcrumb>
-          <div style={{ background: '#fff', padding: 24, minHeight: 280 }}>Content</div>
+        <Content style={{ padding: '50px' }}>
+          <div style={{ background: '#fff', padding: '50px', minHeight: 280 }}>
+            <Switch>
+              <Route exact path='/' component={Home} />
+            </Switch>
+          </div>
         </Content>
         <Footer style={{ textAlign: 'center' }}>
           Ant Design ©2016 Created by Ant UED
