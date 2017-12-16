65a55a83912eecf817b67524910aa3b241dbb001
Rawipol Wajanavisit <oakraw@gmail.com>
2017-12-16 10:22:57 +0700

    add date param in schedule page

--- src/pages/Schedule.js
+++ src/pages/Schedule.js
@@ -1,11 +1,28 @@
 import React, { Component } from 'react';
 import { Calendar } from 'antd';
+import moment from 'moment';
 
 export default class Schedule extends Component {
+
+    state = {
+        date: 10
+    };
+
+    componentDidMount() {
+        const date = parseInt(this.props.match.params.date, 10)
+        this.setState((prevState) => {
+            return {
+                ...prevState,
+                date,
+            };
+        });
+    }
+
     render() {
         return (
             <div>
-                <Calendar/>
+                <h1>{ this.state.date }</h1>
+                <Calendar value={moment(`2017-01-${this.state.date}`)}/>
             </div>
         )
     }
